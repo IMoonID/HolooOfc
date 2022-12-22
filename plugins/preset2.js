8974c08be4615735a3399c2bbee256ae347bb0cf
IMoonID <120606686+IMoonID@users.noreply.github.com>
2022-12-22 08:24:26 +0700

    Update preset2.js

--- plugins/preset2.js
+++ plugins/preset2.js
@@ -2,11 +2,18 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn }) => {
   let caption = `
 *「 PRESET 2 」*
-https://alight.link/yFBAfi6X845BjVuM9
+Xml:
+https://drive.google.com/file/d/1GzHNdvYHXIPjcD0KgorYODyLVQwZ-PfC/view?usp=drivesdk
+
+Via alight motion link:
+https://alight.link/g9rRtnFtEw5gvvPb9
+
+Source Link:
+https://vt.tiktok.com/ZS826pasL/
 `.trim()
   m.reply(caption)
 }
-handler.help = ['Preset AM']
+handler.help = ['PresetAm']
 handler.tags = ['tools']
 handler.command = /^(p2)$/i
 handler.limit = true
