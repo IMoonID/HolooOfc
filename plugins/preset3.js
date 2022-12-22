c9f052d1c94c0c29fcd525c5cd1269f21384d48e
IMoonID <120606686+IMoonID@users.noreply.github.com>
2022-12-22 08:26:25 +0700

    Update preset3.js

--- plugins/preset3.js
+++ plugins/preset3.js
@@ -2,11 +2,18 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn }) => {
   let caption = `
 *「 PRESET 3 」*
-https://alight.link/zfrRNimRJHE7XrNd7
+Xml:
+https://drive.google.com/file/d/1QN-mBmdgFzALnMIymtiGABpCSz9zo3X3/view?usp=drivesdk
+
+5 MB:
+https://alight.link/FcN3ME55YowSDtZFA
+
+Source link:
+https://vt.tiktok.com/ZS826TPN1/
 `.trim()
   m.reply(caption)
 }
-handler.help = ['Preset AM']
+handler.help = ['PresetAm']
 handler.tags = ['tools']
 handler.command = /^(p3)$/i
 handler.limit = true
