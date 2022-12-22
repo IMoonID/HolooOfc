b3f49dec95615828a3aa07863a069de5d5f11373
IMoonID <120606686+IMoonID@users.noreply.github.com>
2022-12-22 20:05:40 +0700

    Update maker-logosadboy.js

--- plugins/maker-logosadboy.js
+++ plugins/maker-logosadboy.js
@@ -4,11 +4,11 @@ let response = args.join(' ').split('|')
   if (!args[0]) throw 'Masukkan Parameter'
   m.reply('proses..')
   let res = `https://ziy.herokuapp.com/api/maker/sadboy?text1=${response[0]}&text2=${response[1]}&apikey=xZiyy`
-  conn.sendFile(m.chat, res, 'gfx3.jpg', `Nih kak`, m, false)
+  conn.sendFile(m.chat, res, 'sadboy.jpg', `Nih kak`, m, false)
 }
 handler.help = ['sadboylogo'].map(v => v + ' <text|text>')
 handler.tags = ['maker']
 handler.command = /^(logosadboy)$/i
-handler.limit = true
+handler.limit = false
 
 export default handler
