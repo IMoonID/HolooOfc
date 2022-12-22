fd4637ee3659c8b950dbaefea9e80dbeb2f5aabc
IMoonID <120606686+IMoonID@users.noreply.github.com>
2022-12-22 08:49:05 +0700

    Update anime_husbu.js

--- plugins/anime_husbu.js
+++ plugins/anime_husbu.js
@@ -1,13 +1,13 @@
 import fetch from 'node-fetch'
 
-let handler = async (m, { conn, command, usedPrefix }) => {
-	let url = await fetch(`https://restapi.frteam.xyz/husbu?&apikey=Arifzyn19`)
-	let res = await url.json()
-	let cap = `_${sym} Random Picture_ :> ${command}`
-	conn.sendButtonImg(m.chat, res.url, cap, sym2 + ' Charater Name : ' + res.nama, 'Next', usedPrefix + 'husbu', m)
+let handler = async (m, { conn, command }) => {
+	let url = 'https://restapi.frteam.xyz/husbu?&apikey=Arifzyn19'
+	conn.sendButton(m.chat, 'Nih husbu nya', wm, await(await fetch(url)).buffer(), [['Next Husbu',`.${command}`]],m)
 }
 handler.command = /^(husbu)$/i
 handler.tags = ['anime']
 handler.help = ['husbu']
+handler.premium = false
 handler.limit = true
+
 export default handler
