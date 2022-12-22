a9c9d3ace911d27d0e107d043492bdaff89c6e8a
IMoonID <120606686+IMoonID@users.noreply.github.com>
2022-12-22 10:39:46 +0700

    Update info-rules.js

--- plugins/info-rules.js
+++ plugins/info-rules.js
@@ -1,31 +1,103 @@
-import fetch from "node-fetch"
-import { generateWAMessageFromContent } from "@adiwajshing/baileys"
+import fs from 'fs'
+import moment from 'moment-timezone'
+let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
+let tag = `@${m.sender.replace(/@.+/, '')}`
+  let mentionedJid = [m.sender]
+let name = conn.getName(m.sender)
+await m.reply(`*_${md} @${m.sender.split(`@`)[0]}..._*`)
+let esce = `text nya `
+let flaaa2 = [
+'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
+'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
+'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=']
+let rules = `➯ *Tɪᴅᴀᴋ Sᴘᴀᴍ Bᴏᴛ*
+ _Kebijakan privasi atau Private without being in public_
 
-let handler  = async (m, { conn }) => {
 
-  let pp = 'https://tinyurl.com/24u64tky'
-  try {
-    pp = await conn.profilePictureUrl(m.sender, 'image')
-  } catch (e) {
+
+• *Kebijakan Privasi:*
+1. Bot tidak akan merekam data riwayat chat user.
+2. Bot tidak akan menyebarkan nomor users.
+3. Bot tidak akan menyimpan media yang dikirimkan oleh users.
+4. Bot tidak akan menyalah gunakan data data users.
+5. Owner HoloBot berhak melihat data riwayat chat users.
+6. Bot berhak melihat status users.
+7. Bot dapat melihat riwayat chat, dan media yang dikirimkan users.
+
+• Jika ada bug/eror di website kami saya mohon untuk Report nya, tanpa biaya dan aman
+
+_Cara penggunaan Holo Bot Agar terhindar dari Banned_
+
+• *Peraturan Holo Bot:*
+1. Users dilarang menelpon maupun memvideo call nomor bot.
+2. Users dilarang mengirimkan berbagai bug, virtex, dll ke nomor bot.
+3. Users diharap tidak melakukan spam dalam penggunaan bot.
+4. Users dilarang menambahkan nomor bot secara illegal, untuk menambahkan silahkan hubungi Owner.
+5. Users diharap untuk tidak menyalah gunakan fitur fitur bot.
+
+• *Note:*
+1. Jika ada yang menjual/beli/sewa bot atas nomor ini, harap segera hubungi owner!
+2. Jika ingin donasi bisa langsung aja ya social payment Dana 
+3. jika ingin membeli scrip bot Whatsapp bisa langsung Hubungi owner
+3. Ketik .sewa Jika Ingin SewaBot 
+
+•Agar terhindar dari Suspand/Ban kalian bisa membaca juga di Peraturan kami.
+
+•Perlu kalian tahu bahwa kami menjaga Privasi dari data-data anda!
+
+• *Syarat Ketentuan Bot:*
+
+1. HoloBot akan keluar dari group jika ada salah satu member melanggar peraturan.
+2. HoloBot-MD dapat mem-ban users secara sepihak terlepas dari users salah atau tidak.
+3. HoloBot tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.
+4. HoloBot akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
+5. HoloBot bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
+❏┳━━◩
+┍┛
+┆⟥⟤ ➠ ${global.bottime}
+└─┈⟅
+${global.botdate}`
+let nth = `☰⟥⟝⟞⟝❨ *Rᴜʟᴇs Bᴏᴛ* ❩⟞⟝⟞⟤☰`
+conn.send3ButtonImg(m.chat, `${pickRandom(flaaa2)}` + `${ucapan()} ` + `${name}`, nth, rules, 'Menu', '.menu', 'Owner', '.owner', 'Credit', '.credit', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: 'nekopoi.care',
+    mediaType: 2, 
+    description: sgc,
+    title: "Halo Kak",
+    body: wm,
+    thumbnail: fs.readFileSync('./thumbnail.jpg'),
+    sourceUrl: 'nekopoi.care',
+     }}
+  })
+}
+handler.help = ['rules']
+handler.tags = ['info']
+handler.command = /^(rules|rule)$/i
+
+export default handler
+
+function ucapan() {
+  const time = moment.tz('Asia/Jakarta').format('HH')
+  let res = "Selamat Malam"
+  if (time >= 4) {
+    res = "Selamat Pagi"
   }
-
-let msg = await generateWAMessageFromContent(m.chat, { locationMessage: {
-  degreesLatitude: 0,
-  degreesLongitude: 0,
-  name: '𝗖𝗟𝗜𝗖𝗞 𝗛𝗘𝗥𝗘',
-  address: 'Lihat Rules Bot',
-  url: 'https://fangzbot.websites.co.in/update/persyaratan/1238529',
-  isLive: true,
-  accuracyInMeters: 0,
-  speedInMps: 0,
-  degreesClockwiseFromMagneticNorth: 2,
-  comment: '',
-  jpegThumbnail: await( await fetch(thumb)).buffer()
-}}, { quoted: m })
-
-return conn.relayMessage(m.chat, msg.message, {})
+  if (time >= 10) {
+    res = "Selamat Siang"
+  }
+  if (time >= 15) {
+    res = "Selamat Sore"
+  }
+  if (time >= 18) {
+    res = "Selamat Malam"
+  }
+  return res
 }
 
-handler.command = /^rules$/
-handler.owner = false
-export default handler
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)]
+}
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
