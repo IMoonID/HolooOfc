febaccd2e3cd60d9130c18086d043d60a51349c4
IMoonID <120606686+IMoonID@users.noreply.github.com>
2022-12-22 08:17:31 +0700

    Update preset1.js

--- plugins/preset1.js
+++ plugins/preset1.js
@@ -2,11 +2,17 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn }) => {
   let caption = `
 *「 PRESET 1 」*
-https://alight.link/jFQ3BdcHfxEmfTFbA
+https://alight.link/d1Q2Qm87dD2dabAk9
+
+Xml:
+https://drive.google.com/file/d/1xtGdxysSOC8mf71qJvZMsGJCAQy9lueH/view?usp=drivesdk
+
+Source:
+https://vt.tiktok.com/ZS826cgD1/
 `.trim()
   m.reply(caption)
 }
-handler.help = ['Preset AM']
+handler.help = ['PresetAm']
 handler.tags = ['tools']
 handler.command = /^(p1)$/i
 handler.limit = true
