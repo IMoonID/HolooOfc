41590ae82005418a0e5072077130a0826c08fefb
IMoonID <120606686+IMoonID@users.noreply.github.com>
2022-12-22 09:19:40 +0700

    Update fun-dadu.js

--- plugins/fun-dadu.js
+++ plugins/fun-dadu.js
@@ -1,11 +1,6 @@
 /* CREDITS TO https://github.com/FG98F */
 const dir = [
-  'https://tinyurl.com/ygms8wvy',
-  'https://tinyurl.com/yhdyhnap',
-  'https://tinyurl.com/yfwjbou7',
-  'https://tinyurl.com/yh3e3ogt',
-  'https://tinyurl.com/yfmhpvxs',
-  'https://tinyurl.com/ygpxka9q'
+  'https://saipulanuar.ga/api/maker/dadu',
 ];
 let handler = async (m, { conn }) => {
 conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dadu.webp', '', m)
