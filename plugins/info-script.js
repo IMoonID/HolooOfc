54dfee2364cd6836686d77e66350b7183d45f95f
IMoonID <120606686+IMoonID@users.noreply.github.com>
2022-12-22 09:03:53 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,33 +1,77 @@
-let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
-let imgr = flaaa.getRandom()
-let text = `*SYARAT & KETENTUAN*
+let { MessageType } = (await import('@adiwajshing/baileys')).default
 
-1. Jangan diperjual belikan Script ini
-2. Jangan salah gunakan script ini!
-3. Jangan di per jual belikan
+let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+  let type = (args[0] || '').toLowerCase()
+  let _type = (args[0] || '').toLowerCase()
 
--------- Tqto ---------
-Allah SWT
-My parents
-All Friends
-All Contributors
-All Creator Bot
-Adiwajshing
-Nurutomo
-BochilGaming`
-const templateButtons = [
-    {index: 1, urlButton: {displayText: 'Script di sini', url: 'https://wa.me/0'}},
+//------- NOMOR
+  let nowner = `${nomorown.split`@`[0]}@s.whatsapp.net`
+  let teksnomor = `${htki} *OWNER* ${htka}
+✦ @${nomorown.split`@`[0]} ✦
+------- ${nameown} -------
+
+📮 *Note:*
+• Owner tidak menerima save contact
+• Owner berhak blockir tanpa alasan
+• Berbicaralah yang sopan & tidak spam
+• Owner Hanya merespon yang berkaitan dengan BOT
+• No Telp`
+
+  let teks = `Menampilkan Source Code Dari Holo Bot\n\nRecode Oleh Sho`
+const sections = [
+   {
+	title: `${htjava} Script Bot –––––––––·•`,
+	rows: [
+	{title: "Base Sc: https://github.com/IMoonID/MooonID", rowId: "yooooo"},
 ]
-let tm = {
-text: text,
-footer: global.bottime,
-templateButtons: templateButtons,
-image: `${imgr + 'Script'}`,
+    },{
+	title: `Lainnya`,
+	rows: [
+	    {title: "Owner Bot", rowId: ".owner nomor"},
+	{title: "Back To Menu", rowId: ".menu"},
+	{title: "Sewa Bot", rowId: ".sewa"},
+	]
+  },
+]
+
+const listMessage = {
+  text: teks,
+  footer: null,
+  title: `Source Code Bot`,
+  buttonText: "Klik",
+  sections
 }
-conn.sendMessage(m.chat, tm, m)
+
+  try {
+    if (/(presetmenu)/i.test(command)) {
+      const count = args[1] && args[1].length > 0 ? Math.min(99999999, Math.max(parseInt(args[1]), 1)) : !args[1] || args.length < 3 ? 1 : Math.min(1, count)
+        switch (type) {
+          case 'nomor':
+          conn.reply(m.chat, teksnomor, m, { contextInfo: { mentionedJid: [nowner] }})
+            break
+          default:
+            return await conn.sendMessage(m.chat, listMessage, m, { contextInfo: { mentionedJid: [m.sender] }})
+        }
+    } else if (/enchant|enchan/i.test(command)) {
+      const count = args[2] && args[2].length > 0 ? Math.min(99999999, Math.max(parseInt(args[2]), 1)) : !args[2] || args.length < 4 ? 1 :Math.min(1, count)
+      switch (_type) {
+        case 't':
+          break
+        case '':
+          break
+
+        default:
+          return conn.sendButton( m.chat, caption, wm, null, [`⋮Back To Menu`, `.menu`], m)
+      }
+    }
+  } catch (err) {
+    m.reply("Error\n\n\n" + err.stack)
+  }
 }
-handler.help = ['sc'] 
- handler.tags = ['info'] 
- handler.command = /^(s(ourcode|c))$/i 
-  
- export default handler
+
+handler.help = ['sourcecode']
+handler.tags = ['main']
+handler.command = /^(sc|sourcecode|scbot)/i
+
+
+export default handler
