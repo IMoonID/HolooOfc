e39c27ebe9284bc29326816b5f346e1c1b99810f
IMoonID <120606686+IMoonID@users.noreply.github.com>
2022-12-23 11:42:55 +0700

    Create anonymouschat.js

---
+++ plugins/anonymouschat.js
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn }) => {
+  let caption = `
+乂 Anonymous Chat
+
+Ketik #start untuk memulai
+Note: Tunggu Partner anonymous cukup lama jadi sabar
+
+Ketik #leave untuk keluar
+
+Ketik #skip untuk skip
+
+Ketik #sendkontak untuk mengirimkan kontak kita ke
+Partner anon chat
+
+ ⚠️ Laporkan Jika ada orang yang mengirimkan aneh²
+`.trim()
+  m.reply(caption)
+}
+handler.help = ['anonchat']
+handler.tags = ['anonymous']
+handler.command = /^(anonchat|anonymouschat)$/i
+handler.limit = true
+
+export default handler
