799f323905ddc2402fda1b3af2f123bfa5d550be
IMoonID <120606686+IMoonID@users.noreply.github.com>
2022-12-23 11:01:05 +0700

    Update rpg-monthly.js

--- plugins/rpg-monthly.js
+++ plugins/rpg-monthly.js
@@ -13,7 +13,7 @@ let handler = async (m, {usedPrefix}) => {
 '*–––––『 COOLDOWN 』–––––*',
 `Kamu sudah claim monthly, mohon tunggu cooldown selesai
 
-🕰️️ ${((user.lastmonthly + cooldown) - new Date()).toTimeString()}`.trim(), 'https://telegra.ph/file/f57b63009fbe9fa333747.jpg', [[`Back to menu`, `${usedPrefix}valor`]], m, {asLocation: false})
+🕰️️ ${((user.lastmonthly + cooldown) - new Date()).toTimeString()}`.trim(), 'https://telegra.ph/file/ddcd46b6985df7e5fa8fb.jpg', [[`Back to menu`, `${usedPrefix}valor`]], m, {asLocation: false})
     let text = ''
     for (let reward of Object.keys(rewards)) if (reward in user) {
         user[reward] += rewards[reward]
@@ -22,7 +22,7 @@ let handler = async (m, {usedPrefix}) => {
     conn.sendButton(m.chat,
 `*––––––『 Monthly 』––––––*`,
 `🪴 Monthly Reward :
-${text}`.trim(), 'https://telegra.ph/file/961fe62d65209c227f0d9.jpg', [
+${text}`.trim(), 'https://telegra.ph/file/e95dfa66c892d44362cc2.jpg', [
 [`My Inventory`, `${usedPrefix}inventory`],
 [`Back To Menu`, `${usedPrefix}menu`]
 ], m, {asLocation: false})
