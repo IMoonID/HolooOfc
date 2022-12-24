af8413e3fd0c3108bceee99b88e7611a6f3582e7
IMoonID <120606686+IMoonID@users.noreply.github.com>
2022-12-24 15:25:14 +0700

    Update rpg-transfer.js

--- plugins/rpg-transfer.js
+++ plugins/rpg-transfer.js
@@ -1,7 +1,7 @@
 const items = [
     'money', 'bank', 'potion', 'trash', 'wood',
     'rock', 'string', 'petFood', 'emerald',
-    'diamond', 'gold', 'iron', 'common',
+    'limit', 'gold', 'iron', 'common',
     'uncommon', 'mythic', 'legendary', 'pet',
 ]
 let confirmation = {}
@@ -10,7 +10,7 @@ async function handler(m, { conn, args, usedPrefix, command }) {
     let user = global.db.data.users[m.sender]
     const item = items.filter(v => v in user && typeof user[v] == 'number')
     let lol = `Use format ${usedPrefix}${command} [type] [value] [number]
-example ${usedPrefix}${command} money 9999 @62000000
+example ${usedPrefix}${command} limit 9 @62000000
 
 Item yang bisa di transfer
 ${item.map(v => `${rpg.emoticon(v)}${v}`.trim()).join('\n')}
