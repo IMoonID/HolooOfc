7751223099a3b1b9a466563304eeae4f003002af
IMoonID <120606686+IMoonID@users.noreply.github.com>
2022-12-25 13:51:36 +0700

    Update rpg-upgrade.js

--- plugins/rpg-upgrade.js
+++ plugins/rpg-upgrade.js
@@ -51,7 +51,7 @@ untuk mendapatkannya ketik *${usedPrefix}craft fishingrod*`
                 if (fishingrod > 9) return conn.sendButton(m.chat, `*${rpg.emoticon('fishingrod')}FishingRod* kamu sudah level max`, wm, 'inventory', usedPrefix + 'inv', m)
                 let _kayu = fishingrod * 25
                 let _string = fishingrod * 15
-                let _money = fishingrod * 10000
+                let _money = fishingrod * 900000
                 if (kayu < _kayu || string < _string || money < _money) return conn.sendButton(m.chat, `Material kamu kurang!!${kayu < _kayu ? `\n${rpg.emoticon('kayu')}Kayu Kamu Kurang *${_kayu - kayu}*` : ''}${string < _string ? `\n${rpg.emoticon('string')}String Kamu Kurang *${_string - string}*` : ''}${user.money < _money ? `\n${rpg.emoticon('money')}Uang Kamu Kurang *${_money - money}*` : ''}`, wm, 'inventory', usedPrefix + 'inv', m)
                 user.fishingrod += 1
                 user.kayu -= _kayu * 1
@@ -79,7 +79,7 @@ untuk memilikinya ketik *${usedPrefix}craft Pickaxe*`
                 if (pickaxe > 9) return conn.sendButton(m.chat, `*${rpg.emoticon('pickaxe')}Pickaxe* kamu sudah level max!!`, wm, 'inventory', usedPrefix + 'inv', m)
                 let __batu = pickaxe * 25
                 let __kayu = pickaxe * 15
-                let __money = pickaxe * 15000
+                let __money = pickaxe * 950000
                 if (batu < __batu || kayu < __kayu || money < __money) return conn.sendButton(m.chat, `
 Material Anda Kurang!!
 ${batu < __batu ? `\n${rpg.emoticon('batu')}Batu kamu kurang *${__batu - batu}*` : ''}${kayu < __kayu ? `\n${rpg.emoticon('kayu')}Kayu kamu kurang *${__kayu - kayu}*` : ''}${money < __money ? `\n${rpg.emoticon('money')}Uang kamu kurang *${__money - money}*` : ''}`, wm, 'cek inventory', usedPrefix + 'inv', m)
@@ -109,7 +109,7 @@ untuk memilikinya ketik *${usedPrefix}craft sword*`
                 if (sword > 9) return conn.sendButton(m.chat, `*${rpg.emoticon('sword')}Sword* kamu sudah level max!!`, wm, 'inventory', usedPrefix + 'inv', m)
                 let _iron = sword * 25
                 let ___kayu = sword * 15
-                let ___money = sword * 10000
+                let ___money = sword * 900000
                 if (iron < _iron || kayu < ___kayu || money < ___money) return conn.sendButton(m.chat, `
 Material Anda Kurang!!
 ${iron < _iron ? `\n${rpg.emoticon('iron')}Iron kamu kurang *${_iron - iron}*` : ''}${kayu < ___kayu ? `\n${rpg.emoticon('kayu')}Kayu kamu kurang *${___kayu - kayu}*` : ''}${money < ___money ? `\n${rpg.emoticon('money')}Uang kamu kurang *${___money - money}*` : ''}`, wm, 'inventory', usedPrefix + 'inv', m)
