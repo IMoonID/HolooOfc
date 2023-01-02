3d2785e3c21a53c0ee737218fae657d0d9e9a91a
IMoonID <120606686+IMoonID@users.noreply.github.com>
2023-01-02 11:44:43 +0700

    Update limit.js

--- plugins/limit.js
+++ plugins/limit.js
@@ -3,7 +3,7 @@ let handler = async (m) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     if (typeof db.data.users[who] == 'undefined') throw 'Pengguna tidak ada didalam data base'
-    m.reply(`Kamu Mempunyai ${global.db.data.users[who].limit} Limit\n\nKetik #buy limit untuk membeli limit`)
+    m.reply(`\n\n\nKamu Mempunyai ${global.db.data.users[who].limit} Limit\n\n\n\n`)
 }
 handler.help = ['limit [@user]']
 handler.tags = ['xp']
